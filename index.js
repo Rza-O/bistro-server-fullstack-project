028a0eeb9abf52fcc3747cad1e10b897f59aec7f
appoclayptor <shahreza1414@gmail.com>
2025-01-03 20:13:10 +0600

    added endpoints for cart collection api

--- index.js
+++ index.js
@@ -15,35 +15,51 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-   serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-   }
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	}
 });
 
 async function run() {
-   try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
 
-      const menuCollection = client.db('BistroDB').collection('menu');
+		const menuCollection = client.db('BistroDB').collection('menu');
+		const cartsCollection = client.db('BistroDB').collection('carts');
 
-      app.get('/menu', async (req, res) => {
-         const result = await menuCollection.find().toArray();
-         res.send(result)
-      })
+		app.get('/menu', async (req, res) => {
+			const result = await menuCollection.find().toArray();
+			res.send(result)
+		})
+
+		// carts collection
+
+		app.get('/carts', async (req, res) => {
+			const {email} = req.query;
+			const query = { email: email };
+			const result = await cartsCollection.find(query).toArray();
+			res.send(result);
+		})
+
+		app.post('/carts', async (req, res) => {
+			const cartItem = req.body;
+			const result = await cartsCollection.insertOne(cartItem);
+			res.send(result);
+		})
 
 
 
 
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-   } finally {
-      // Ensures that the client will close when you finish/error
-      // await client.close();
-   }
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
 }
 run().catch(console.dir);
 
@@ -51,10 +67,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req,res) => {
-   res.send('Bistro Restro is running')
+app.get('/', (req, res) => {
+	res.send('Bistro Restro is running')
 })
 
 app.listen(port, () => {
-   console.log(`Bistro is running on ${port}`);
+	console.log(`Bistro is running on ${port}`);
 })

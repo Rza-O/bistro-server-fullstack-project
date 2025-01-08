aebac5494712452e1ff22d545ee2d575fb09a9f3
appoclayptor <shahreza1414@gmail.com>
2025-01-08 15:51:44 +0600

    add email check to user creation API to prevent duplicates

--- index.js
+++ index.js
@@ -35,8 +35,14 @@ async function run() {
 
 
 		// users related api
-		app.post('/users', async (req,res) => {
+		app.post('/users', async (req, res) => {
 			const user = req.body;
+			// insert email if user is new
+			const query = { email: user.email };
+			const existingUser = await usersCollection.findOne(query); 
+			if (existingUser) {
+				return res.send({ message: 'User already exists', insertedId: null });
+			}
 			const result = await usersCollection.insertOne(user);
 			res.send(result);
 		})

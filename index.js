afde55539f0cf74494d3af6e9ca02239d862a228
appoclayptor <shahreza1414@gmail.com>
2025-01-12 08:10:02 +0600

    add payments API to handle payment processing and cart item deletion

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ async function run() {
 		const menuCollection = client.db('BistroDB').collection('menu');
 		const cartsCollection = client.db('BistroDB').collection('carts');
 		const usersCollection = client.db('BistroDB').collection('users');
+		const paymentsCollection = client.db('BistroDB').collection('payments');
 
 
 
@@ -195,6 +196,24 @@ async function run() {
 			})
 		})
 
+
+		// payment after confirmation related api
+		app.post('/payments', async (req, res) => {
+			const payment = req.body;
+			const paymentResult = await paymentsCollection.insertOne(payment);
+			// carefully delete each item from the cart
+			console.log('payment info', payment)
+			const query = {
+				_id: {
+					$in: payment.cartIds.map((id) => new ObjectId(id))
+				}
+			};
+
+			const deleteResult = await cartsCollection.deleteMany(query);
+
+			
+			res.send({paymentResult, deleteResult});
+		})
 
 
 		// Send a ping to confirm a successful connection
